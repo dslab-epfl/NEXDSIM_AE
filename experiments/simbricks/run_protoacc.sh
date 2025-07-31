814de6454d242446e300b7a08e82d8a70c863177
Jonas Kaufmann <jokau03@gmail.com>
2025-07-31 22:47:30 +0000

    experiments/simbricks/run_protoacc.sh: cleanup and launch in tmux windows instead of separate sessions

--- experiments/simbricks/run_protoacc.sh
+++ experiments/simbricks/run_protoacc.sh
@@ -1,34 +1,23 @@
-#make -C .. clean
-#make -C .. -j
+#!/bin/bash
 
-# make -C $HOME/SimBricks-LPN/sims/misc/protoacc/simbricks
+SESSION="run_protoacc"
+BENCHES=(0 1 2 3 4 5)
 
+tmux new-session -d -s "$SESSION" -n "bench${BENCHES[0]}-rtl" \
+  "simbricks-run --verbose --force --filter=protoacc_benchmark-rtl-gem5_o3-bench0 --force ./test_protoacc.py; bash"
+echo bench${BENCHES[0]}-rtl started
 
-tmux new-session -d -s "bench0-rtl" "simbricks-run --verbose --force --filter="protoacc_benchmark-rtl-gem5_o3-bench0" --force ./test_protoacc.py "
-tmux new-session -d -s "bench1-rtl" "simbricks-run --verbose --force --filter="protoacc_benchmark-rtl-gem5_o3-bench1" --force ./test_protoacc.py "
-tmux new-session -d -s "bench2-rtl" "simbricks-run --verbose --force --filter="protoacc_benchmark-rtl-gem5_o3-bench2" --force ./test_protoacc.py "
-tmux new-session -d -s "bench3-rtl" "simbricks-run --verbose --force --filter="protoacc_benchmark-rtl-gem5_o3-bench3" --force ./test_protoacc.py "
-tmux new-session -d -s "bench4-rtl" "simbricks-run --verbose --force --filter="protoacc_benchmark-rtl-gem5_o3-bench4" --force ./test_protoacc.py "
-tmux new-session -d -s "bench5-rtl" "simbricks-run --verbose --force --filter="protoacc_benchmark-rtl-gem5_o3-bench5" --force ./test_protoacc.py "
+for i in "${BENCHES[@]:1}"; do
+  tmux new-window -t "$SESSION" -n "bench$i-rtl" \
+    "simbricks-run --verbose --force --filter=protoacc_benchmark-rtl-gem5_o3-bench$i --force ./test_protoacc.py; bash"
+  echo bench${i}-rtl started
+done
 
-tmux new-session -d -s "bench0-lpn" "simbricks-run --verbose --force --filter="protoacc_benchmark-lpn-gem5_o3-bench0" --force ./test_protoacc.py "
-tmux new-session -d -s "bench1-lpn" "simbricks-run --verbose --force --filter="protoacc_benchmark-lpn-gem5_o3-bench1" --force ./test_protoacc.py "
-tmux new-session -d -s "bench2-lpn" "simbricks-run --verbose --force --filter="protoacc_benchmark-lpn-gem5_o3-bench2" --force ./test_protoacc.py "
-tmux new-session -d -s "bench3-lpn" "simbricks-run --verbose --force --filter="protoacc_benchmark-lpn-gem5_o3-bench3" --force ./test_protoacc.py "
-tmux new-session -d -s "bench4-lpn" "simbricks-run --verbose --force --filter="protoacc_benchmark-lpn-gem5_o3-bench4" --force ./test_protoacc.py "
-tmux new-session -d -s "bench5-lpn" "simbricks-run --verbose --force --filter="protoacc_benchmark-lpn-gem5_o3-bench5" --force ./test_protoacc.py "
+for i in "${BENCHES[@]}"; do
+  tmux new-window -t "$SESSION" -n "bench$i-lpn" \
+    "simbricks-run --verbose --force --filter=protoacc_benchmark-lpn-gem5_o3-bench$i --force ./test_protoacc.py; bash"
+  echo bench${i}-lpn started
+done
 
-# tmux new-session -d -s "bench0-rtl" "cd $HOME/SimBricks-LPN/experiments && python3 run.py --verbose --force --filter="protoacc_benchmark-rtl-gem5_o3-bench0" --force pyexps/test_protoacc.py --repo $HOME/SimBricks-LPN/"
-# tmux new-session -d -s "bench1-rtl" "cd $HOME/SimBricks-LPN/experiments && python3 run.py --verbose --force --filter="protoacc_benchmark-rtl-gem5_o3-bench1" --force pyexps/test_protoacc.py --repo $HOME/SimBricks-LPN/"
-# tmux new-session -d -s "bench2-rtl" "cd $HOME/SimBricks-LPN/experiments && python3 run.py --verbose --force --filter="protoacc_benchmark-rtl-gem5_o3-bench2" --force pyexps/test_protoacc.py --repo $HOME/SimBricks-LPN/"
-# tmux new-session -d -s "bench3-rtl" "cd $HOME/SimBricks-LPN/experiments && python3 run.py --verbose --force --filter="protoacc_benchmark-rtl-gem5_o3-bench3" --force pyexps/test_protoacc.py --repo $HOME/SimBricks-LPN/"
-# tmux new-session -d -s "bench4-rtl" "cd $HOME/SimBricks-LPN/experiments && python3 run.py --verbose --force --filter="protoacc_benchmark-rtl-gem5_o3-bench4" --force pyexps/test_protoacc.py --repo $HOME/SimBricks-LPN/"
-# tmux new-session -d -s "bench5-rtl" "cd $HOME/SimBricks-LPN/experiments && python3 run.py --verbose --force --filter="protoacc_benchmark-rtl-gem5_o3-bench5" --force pyexps/test_protoacc.py --repo $HOME/SimBricks-LPN/"
-
-# tmux new-session -d -s "bench0-lpn" "cd $HOME/SimBricks-LPN/experiments && python3 run.py --verbose --force --filter="protoacc_benchmark-lpn-gem5_o3-bench0" --force pyexps/test_protoacc.py --repo $HOME/SimBricks-LPN/"
-# tmux new-session -d -s "bench1-lpn" "cd $HOME/SimBricks-LPN/experiments && python3 run.py --verbose --force --filter="protoacc_benchmark-lpn-gem5_o3-bench1" --force pyexps/test_protoacc.py --repo $HOME/SimBricks-LPN/"
-# tmux new-session -d -s "bench2-lpn" "cd $HOME/SimBricks-LPN/experiments && python3 run.py --verbose --force --filter="protoacc_benchmark-lpn-gem5_o3-bench2" --force pyexps/test_protoacc.py --repo $HOME/SimBricks-LPN/"
-# tmux new-session -d -s "bench3-lpn" "cd $HOME/SimBricks-LPN/experiments && python3 run.py --verbose --force --filter="protoacc_benchmark-lpn-gem5_o3-bench3" --force pyexps/test_protoacc.py --repo $HOME/SimBricks-LPN/"
-# tmux new-session -d -s "bench4-lpn" "cd $HOME/SimBricks-LPN/experiments && python3 run.py --verbose --force --filter="protoacc_benchmark-lpn-gem5_o3-bench4" --force pyexps/test_protoacc.py --repo $HOME/SimBricks-LPN/"
-# tmux new-session -d -s "bench5-lpn" "cd $HOME/SimBricks-LPN/experiments && python3 run.py --verbose --force --filter="protoacc_benchmark-lpn-gem5_o3-bench5" --force pyexps/test_protoacc.py --repo $HOME/SimBricks-LPN/"
-
+# Attach to the session
+tmux attach-session -t "$SESSION"
