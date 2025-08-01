99e8dfe8236ba3e3536b7b88087e3fdb493bd2bc
Jonas Kaufmann <jokau03@gmail.com>
2025-08-01 06:48:22 +0000

    experiments/simbricks/run_protoacc.sh: undo adding for loops

--- experiments/simbricks/run_protoacc.sh
+++ experiments/simbricks/run_protoacc.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
 SESSION="run_protoacc"
-BENCHES=(0 1 2 3 4 5)
 
-tmux new-session -d -s "$SESSION" -n "bench${BENCHES[0]}-rtl" \
-  "simbricks-run --verbose --force --filter=protoacc_benchmark-rtl-gem5_o3-bench0 --force ./test_protoacc.py; bash"
-echo bench${BENCHES[0]}-rtl started
+tmux new-session -d -s "$SESSION" 
 
-for i in "${BENCHES[@]:1}"; do
-  tmux new-window -t "$SESSION" -n "bench$i-rtl" \
-    "simbricks-run --verbose --force --filter=protoacc_benchmark-rtl-gem5_o3-bench$i --force ./test_protoacc.py; bash"
-  echo bench${i}-rtl started
-done
+tmux new-window -t "$SESSION" -n "bench0-rtl" "simbricks-run --verbose --force --filter=protoacc_benchmark-rtl-gem5_o3-bench0 --force ./test_protoacc.py; bash"
+tmux new-window -t "$SESSION" -n "bench1-rtl" "simbricks-run --verbose --force --filter=protoacc_benchmark-rtl-gem5_o3-bench1 --force ./test_protoacc.py; bash"
+tmux new-window -t "$SESSION" -n "bench2-rtl" "simbricks-run --verbose --force --filter=protoacc_benchmark-rtl-gem5_o3-bench2 --force ./test_protoacc.py; bash"
+tmux new-window -t "$SESSION" -n "bench3-rtl" "simbricks-run --verbose --force --filter=protoacc_benchmark-rtl-gem5_o3-bench3 --force ./test_protoacc.py; bash"
+tmux new-window -t "$SESSION" -n "bench4-rtl" "simbricks-run --verbose --force --filter=protoacc_benchmark-rtl-gem5_o3-bench4 --force ./test_protoacc.py; bash"
+tmux new-window -t "$SESSION" -n "bench5-rtl" "simbricks-run --verbose --force --filter=protoacc_benchmark-rtl-gem5_o3-bench5 --force ./test_protoacc.py; bash"
 
-for i in "${BENCHES[@]}"; do
-  tmux new-window -t "$SESSION" -n "bench$i-lpn" \
-    "simbricks-run --verbose --force --filter=protoacc_benchmark-lpn-gem5_o3-bench$i --force ./test_protoacc.py; bash"
-  echo bench${i}-lpn started
-done
+tmux new-window -t "$SESSION" -n "bench0-lpn" "simbricks-run --verbose --force --filter=protoacc_benchmark-lpn-gem5_o3-bench0 --force ./test_protoacc.py; bash"
+tmux new-window -t "$SESSION" -n "bench1-lpn" "simbricks-run --verbose --force --filter=protoacc_benchmark-lpn-gem5_o3-bench1 --force ./test_protoacc.py; bash"
+tmux new-window -t "$SESSION" -n "bench2-lpn" "simbricks-run --verbose --force --filter=protoacc_benchmark-lpn-gem5_o3-bench2 --force ./test_protoacc.py; bash"
+tmux new-window -t "$SESSION" -n "bench3-lpn" "simbricks-run --verbose --force --filter=protoacc_benchmark-lpn-gem5_o3-bench3 --force ./test_protoacc.py; bash"
+tmux new-window -t "$SESSION" -n "bench4-lpn" "simbricks-run --verbose --force --filter=protoacc_benchmark-lpn-gem5_o3-bench4 --force ./test_protoacc.py; bash"
+tmux new-window -t "$SESSION" -n "bench5-lpn" "simbricks-run --verbose --force --filter=protoacc_benchmark-lpn-gem5_o3-bench5 --force ./test_protoacc.py; bash"
 
 # Attach to the session
 tmux attach-session -t "$SESSION"
