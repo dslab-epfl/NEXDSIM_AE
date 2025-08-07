b740c46a30bdab2528c262f341b48231f5ac3095
MJChku <mjc97cooper@gmail.com>
2025-08-07 18:23:00 +0200

    enable all exp in vta

--- experiments/simbricks/run_vta.sh
+++ experiments/simbricks/run_vta.sh
@@ -7,20 +7,20 @@
 
 # mkdir -p log
 
-# tmux new-session -d -s "vtatest-gt-rtl" "simbricks-run --verbose --force --filter='vtatest-gt-rtl' ./vtatest.py"
-# tmux new-session -d -s "vtatest-gt-lpn" "simbricks-run --verbose --force --filter='vtatest-gt-lpn' ./vtatest.py"
+tmux new-session -d -s "vtatest-gt-rtl" "simbricks-run --verbose --force --filter='vtatest-gt-rtl' ./vtatest.py"
+tmux new-session -d -s "vtatest-gt-lpn" "simbricks-run --verbose --force --filter='vtatest-gt-lpn' ./vtatest.py"
 
-# tmux new-session -d -s "classify_resnet18_rtl" "simbricks-run --verbose --force --filter='classify-resnet18_v1-vta-gem5_o3-rtl-4-2000' --force ./classify_simple.py"
-# tmux new-session -d -s "classify_resnet18_lpn" "simbricks-run --verbose --force --filter='classify-resnet18_v1-vta-gem5_o3-lpn-4-2000' --force ./classify_simple.py"
+tmux new-session -d -s "classify_resnet18_rtl" "simbricks-run --verbose --force --filter='classify-resnet18_v1-vta-gem5_o3-rtl-4-2000' --force ./classify_simple.py"
+tmux new-session -d -s "classify_resnet18_lpn" "simbricks-run --verbose --force --filter='classify-resnet18_v1-vta-gem5_o3-lpn-4-2000' --force ./classify_simple.py"
 
-# tmux new-session -d -s "classify_resnet34_rtl" "simbricks-run --verbose --force --filter='classify-resnet34_v1-vta-gem5_o3-rtl-4-2000' --force ./classify_simple.py"
-# tmux new-session -d -s "classify_resnet34_lpn" "simbricks-run --verbose --force --filter='classify-resnet34_v1-vta-gem5_o3-lpn-4-2000' --force ./classify_simple.py"
+tmux new-session -d -s "classify_resnet34_rtl" "simbricks-run --verbose --force --filter='classify-resnet34_v1-vta-gem5_o3-rtl-4-2000' --force ./classify_simple.py"
+tmux new-session -d -s "classify_resnet34_lpn" "simbricks-run --verbose --force --filter='classify-resnet34_v1-vta-gem5_o3-lpn-4-2000' --force ./classify_simple.py"
 
-# tmux new-session -d -s "classify_resnet50_rtl" "simbricks-run --verbose --force --filter='classify-resnet50_v1-vta-gem5_o3-rtl-4-2000' --force ./classify_simple.py"
-# tmux new-session -d -s "classify_resnet50_lpn" "simbricks-run --verbose --force --filter='classify-resnet50_v1-vta-gem5_o3-lpn-4-2000' --force ./classify_simple.py"
+tmux new-session -d -s "classify_resnet50_rtl" "simbricks-run --verbose --force --filter='classify-resnet50_v1-vta-gem5_o3-rtl-4-2000' --force ./classify_simple.py"
+tmux new-session -d -s "classify_resnet50_lpn" "simbricks-run --verbose --force --filter='classify-resnet50_v1-vta-gem5_o3-lpn-4-2000' --force ./classify_simple.py"
 
-# tmux new-session -d -s "detect_rtl" "simbricks-run --verbose --force --filter='detect-vta-gem5_o3-rtl-2000' --force ./detect_simple.py"
-# tmux new-session -d -s "detect_lpn" "simbricks-run --verbose --force --filter='detect-vta-gem5_o3-lpn-2000' --force ./detect_simple.py"
+tmux new-session -d -s "detect_rtl" "simbricks-run --verbose --force --filter='detect-vta-gem5_o3-rtl-2000' --force ./detect_simple.py"
+tmux new-session -d -s "detect_lpn" "simbricks-run --verbose --force --filter='detect-vta-gem5_o3-lpn-2000' --force ./detect_simple.py"
 
 tmux new-session -d -s "classify_lpn_8" "simbricks-run --verbose --force --filter='classify_multi-resnet18_v1-vta-go3-lpn-8' --force ./classify_multi.py "
 
